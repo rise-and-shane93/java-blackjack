a298c4bd7829b84ca30f8ec11b0718c95a4a68c2
Shane Harper <cdtharper@gmail.com>
2021-03-18 22:05:34 -0400

    add card file

--- BlackjackGameSimulator.java
+++ BlackjackGameSimulator.java
@@ -1,5 +1,7 @@
 public class BlackjackGameSimulator {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Card diamond = new Card();
+
+        diamond.printCardSymbol();
     }
 }
--- Card.java
+++ Card.java
@@ -1,18 +1,50 @@
 public class Card {
 
     private String cardName;
+    private int cardValue;
+    private String cardSymbol;
 
     private static String[] cardArr = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    private static String[] symbols = {"♥", "◆", "♠", "♣"};
     
     public Card() {
-        this.cardName = setCardName()
+        // this.cardName = setCardName();
+        this.cardValue = setCardValue();
+        this.cardSymbol = setCardSymbol();
+        this.cardName = setCardName();
     }
 
-    public String cardValue() {
-        return null;
+    public int setCardValue() {
+        int randomNumberCard = (int) ( cardArr.length * Math.random() );
+
+        String stringArrElement = cardArr[randomNumberCard];
+        int result = 0;
+
+        switch (stringArrElement) {
+            case "2", "3", "4", "5", "6", "7", "8", "9", "10":
+                result = Integer.parseInt(stringArrElement);
+                break;
+            case "J", "Q", "K":
+                result = 10;
+                break;
+            case "A":
+                result = 1;
+                break;
+        }
+        return result;
     }
 
     public String setCardName() {
-        return null;
+        return String.valueOf(this.cardValue) + " " + this.cardSymbol;
+    }
+
+    public String setCardSymbol() {
+        int randomNumberSymbol = (int) ( symbols.length * Math.random() );
+
+        return symbols[randomNumberSymbol];
+    }
+
+    public void printCardSymbol() {
+        System.out.println(this.cardName);
     }
 }
